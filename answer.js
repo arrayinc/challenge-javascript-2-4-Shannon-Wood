894d0d01d478e99a13bfc59da8437c315679940f
Shannon Wood <shannon@4woods.us>
2018-12-05 11:16:56 -0700

    JavaScript Challenge 4 - shaky but complete.

--- answer.js
+++ answer.js
@@ -1,1 +1,12 @@
 // *** YOUR ANSWER BELOW ***
+// ET - this question was sketchy.
+// There was no way to get one correct answer because
+// different consoles return different results.
+// Atom automatically adds single quotes so this result
+// displays triple quotes within Atom.
+
+function quote(x) {
+return '"' + x + '"';
+}
+
+quote ("hello " + 10*10)
